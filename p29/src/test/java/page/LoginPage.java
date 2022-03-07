51de5e65c3443897cb66d4068d058115f426db02
Hp <Hp@Vasanth.lan>
2022-03-07 08:48:38 +0530

    Commit the code

--- p29/src/test/java/page/LoginPage.java
+++ p29/src/test/java/page/LoginPage.java
@@ -7,14 +7,28 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-	@FindBy(id = "username")
+	@FindBy(id="username")
 	private WebElement unTB;
-
+	
+	@FindBy(name="pwd")
+	private WebElement pwTB;
+	
+	@FindBy(xpath = "//div[.='Login ']")
+	private WebElement loginBTN;
+	
 	public LoginPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
-
+	
 	public void setUserName(String un) {
 		unTB.sendKeys(un);
+	}
+	
+	public void setPassword(String pw) {
+		pwTB.sendKeys(pw);
+	}
+	
+	public void clickLoginButton() {
+		loginBTN.click();
 	}
 }
